4e683acbd8eea5976baed72b16ce63793dd7904b
Luis Parada <luisparadawagner@gmail.com>
2014-09-30 15:53:57 -0300

    Primer Commit

---
+++ Negocio/RolesNego.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+using Repositorio;
+
+namespace Negocio
+{
+    public class RolesNego
+    {
+        RolesRepo rolesRepo = new RolesRepo();
+
+        public IEnumerable<SSO_Role> listaRoles(int parent, bool enable)
+        {
+            IList<SSO_Role> rol = rolesRepo.listaRoles().ToList();
+
+            List<SSO_Role> listaRoles = new List<SSO_Role>();
+
+            foreach (SSO_Role data in rol)
+            {
+                if ((data.Parent == parent) && (data.Enabled == enable))
+                {
+                    SSO_Role ssoRol = new SSO_Role();
+
+                    ssoRol.Id = data.Id;
+                    ssoRol.Name = data.Name;
+                    listaRoles.Add(ssoRol);
+                }
+            }
+
+            return listaRoles;
+        }
+
+        public void guardarRol(SSO_Role rol)
+        {
+            rolesRepo.guardarRol(rol);
+        }
+    }
+}
---
+++ Repositorio/RolesRepo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Repositorio
+{
+    public class RolesRepo
+    {
+        ModeloDominio dominio = new ModeloDominio();
+
+        public IEnumerable<SSO_Role> listaRoles()
+        {
+            IEnumerable<SSO_Role> result = dominio.SSO_Roles.ToList();
+
+            return result;
+        }
+
+        public void guardarRol(SSO_Role rol)
+        {
+            dominio.Add(rol);
+            dominio.SaveChanges();
+        }
+    }
+}
